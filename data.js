1b81c2af3dee04858ef02564229d3e83481127aa
Armen Grigorian <info@propertystart.com.au>
2020-04-28 09:57:32 +1000

    Version 1

---
+++ data.js
@@ -0,0 +1,524 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-porch",
+      "name": "Front Porch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.2419026529213326,
+        "pitch": 0.07733903465539527,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.477838798878608,
+          "pitch": 0.16631997051934277,
+          "rotation": 5.497787143782138,
+          "target": "1-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.3198194167961539,
+        "pitch": -0.007238320074435478,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5242904688307704,
+          "pitch": 0.22234619896831376,
+          "rotation": 4.71238898038469,
+          "target": "2-loungedining"
+        },
+        {
+          "yaw": -1.1183943744449873,
+          "pitch": 0.24553788420915268,
+          "rotation": 0,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-loungedining",
+      "name": "Lounge/Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.3818142313028154,
+        "pitch": 0.2022865670839913,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7871265397747855,
+          "pitch": 0.16758511566098733,
+          "rotation": 0.7853981633974483,
+          "target": "0-front-porch"
+        },
+        {
+          "yaw": -2.880907216353254,
+          "pitch": 0.2751635039172733,
+          "rotation": 5.497787143782138,
+          "target": "4-powder-room"
+        },
+        {
+          "yaw": -2.1343220513028207,
+          "pitch": 0.021873932491800474,
+          "rotation": 0,
+          "target": "5-first-floor-landing"
+        },
+        {
+          "yaw": -2.1343220513028207,
+          "pitch": 0.021873932491800474,
+          "rotation": 11.780972450961727,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3099290260755474,
+        "pitch": 0.033284187878802385,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.210628507303106,
+          "pitch": 0.3043339220440462,
+          "rotation": 5.497787143782138,
+          "target": "4-powder-room"
+        },
+        {
+          "yaw": 2.408832285966888,
+          "pitch": 0.18348941532482854,
+          "rotation": 5.497787143782138,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-powder-room",
+      "name": "Powder Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-first-floor-landing",
+      "name": "First Floor Landing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6548403190615808,
+        "pitch": 0.3160687284126773,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5258462124163934,
+          "pitch": 0.7761545215585244,
+          "rotation": 2.356194490192345,
+          "target": "2-loungedining"
+        },
+        {
+          "yaw": 2.38496792098391,
+          "pitch": 0.30061671517091426,
+          "rotation": 0,
+          "target": "6-bedroom-2-with-wir"
+        },
+        {
+          "yaw": -1.6426636759709314,
+          "pitch": 0.16744323857025023,
+          "rotation": 0,
+          "target": "9-master"
+        },
+        {
+          "yaw": -1.9611593113014685,
+          "pitch": 0.44437040027509056,
+          "rotation": 5.497787143782138,
+          "target": "9-master"
+        },
+        {
+          "yaw": 1.0366403229745114,
+          "pitch": 0.2579653653334866,
+          "rotation": 0.7853981633974483,
+          "target": "8-bedroom-3"
+        },
+        {
+          "yaw": 1.3238591796077088,
+          "pitch": 0.4704938402930203,
+          "rotation": 7.853981633974483,
+          "target": "7-first-floor-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bedroom-2-with-wir",
+      "name": "Bedroom 2 with WIR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3373780870161696,
+        "pitch": 0.06532502166094112,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8620307634436752,
+          "pitch": 0.19729838729478466,
+          "rotation": 0.7853981633974483,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-first-floor-bathroom",
+      "name": "First Floor Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0521898555477609,
+          "pitch": 0.45097128979023715,
+          "rotation": 1.5707963267948966,
+          "target": "8-bedroom-3"
+        },
+        {
+          "yaw": -0.39562225214628555,
+          "pitch": 0.10136080907756018,
+          "rotation": 4.71238898038469,
+          "target": "6-bedroom-2-with-wir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bedroom-3",
+      "name": "Bedroom 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.3793312243847335,
+        "pitch": 0.12901691806710502,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.136585285650259,
+          "pitch": 0.20467753900449814,
+          "rotation": 10.995574287564278,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-master",
+      "name": "Master",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.15553392372338948,
+        "pitch": 0.08177666048229071,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5414648348881865,
+          "pitch": 0.26044961238082465,
+          "rotation": 4.71238898038469,
+          "target": "10-master-en-suite"
+        },
+        {
+          "yaw": -2.0303268838705044,
+          "pitch": 0.2062582489769138,
+          "rotation": 0.7853981633974483,
+          "target": "11-master-wir"
+        },
+        {
+          "yaw": -2.3034179500958896,
+          "pitch": 0.31966248179747936,
+          "rotation": 8.63937979737193,
+          "target": "5-first-floor-landing"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-master-en-suite",
+      "name": "Master En-Suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.336313886741003,
+        "pitch": 0.15129150270843716,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-master-wir",
+      "name": "Master WIR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.835511202942909,
+        "pitch": 0.014954408168790678,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "2-5 Claire St McKinnon",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
